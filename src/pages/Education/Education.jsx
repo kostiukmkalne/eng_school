b5ef066ad5c889f520d0083eca4d29daf559c8fd
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2025-02-04 23:07:13 -0600

    project

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Education.scss";
 import { useTranslation } from "react-i18next";
 import { Button } from "../../components/Button/Button.jsx";
